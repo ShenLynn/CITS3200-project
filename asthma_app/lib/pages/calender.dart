1fbcdcb282e7be14e267cf685d1aaecbcc97926c
Sam <22232705@student.uwa.edu.au>
2020-08-25 19:12:26 +0800

    Skeleton beautification v1

--- asthma_app/lib/pages/calender.dart
+++ asthma_app/lib/pages/calender.dart
@@ -12,25 +12,58 @@ class _CalenderState extends State<Calender> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-          child: Row(
-            children: <Widget>[
-              FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, 'profile');
-                },
-                icon: Icon(Icons.person),
-                label: Text("Navigate to user"),
-              ),
-              FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, 'forms_page');
-                },
-                icon: Icon(Icons.insert_drive_file),
-                label: Text("Navigate to forms"),
-              )
-            ],
+          child: Container(
+            color: Colors.green,
+            child: Row(
+              //different rows allow for diff allignment of icon within the parent row.
+              children: [Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    FlatButton.icon(
+                  color:Colors.green,
+                   icon: Icon(
+                    Icons.person,
+                    color:Colors.white),
+                    onPressed: () {
+                    Navigator.pushNamed(context, 'profile');
+                    },
+                      label: Text("")
+                      )],
+                    ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                  //TODO: Add in children w/notiication icon button,
+              )],
+            )
           )
         ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.green,
+        child: Row(
+          children: <Widget>[
+            FlatButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, 'profile');
+              },
+              icon: Icon(
+                  Icons.person, color:Colors.white),
+              label: Text(
+                  "Navigate to user",
+              style:TextStyle(
+                color:Colors.white)),
+            ),
+            FlatButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, 'forms_page');
+              },
+              icon: Icon(Icons.insert_drive_file, color: Colors.white),
+              label: Text(
+                "Navigate to forms",
+              style:TextStyle(color: Colors.white)),
+            )
+          ],
+        )
+      ),
     );
   }
 }
