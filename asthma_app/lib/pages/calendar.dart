8b24c759437e11632f6c365bceeb1efbbcb47713
Sam <22232705@student.uwa.edu.au>
2020-08-29 16:33:20 +0800

    Basic calendar setup

--- asthma_app/lib/pages/calendar.dart
+++ asthma_app/lib/pages/calendar.dart
@@ -2,14 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 //Calendar functions as both a calendar and the default home screen
-
+//setting up home screen
 class Calendar extends StatefulWidget {
   @override
   _CalendarState createState() => _CalendarState();
 }
 
 class _CalendarState extends State<Calendar> {
+  CalendarController _controller;
   @override
+  void initState() {
+    //TODO implement calendar initstate if required
+    super.initState();
+    _controller = CalendarController();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -24,7 +31,7 @@ class _CalendarState extends State<Calendar> {
               Navigator.pushNamed(context, 'profile');
             },
           ),
-          title: Text("Asthma App Alpha"),
+          title: Text("Asthma App [Alpha]"),
           centerTitle: true,
           actions: [
             IconButton(
@@ -33,10 +40,38 @@ class _CalendarState extends State<Calendar> {
                 color:Colors.white
                 ),
                 onPressed: () {
-
+                //TODO: Add function to display notifications
                 },
               )
             ]),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TableCalendar(
+                initialCalendarFormat: CalendarFormat.month,
+              headerStyle: HeaderStyle(
+                formatButtonShowsNext: false,
+                formatButtonDecoration: BoxDecoration(
+                  color:Colors.blue[900],
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                formatButtonTextStyle: TextStyle(
+                  color: Colors.white
+                )
+              ),
+              calendarStyle: CalendarStyle(
+                todayColor: Colors.blue[300],
+                selectedColor: Colors.blue[900]
+              ),
+
+              calendarController: _controller,)
+            ],
+          ),
+        ),
+
+
+
         bottomNavigationBar: BottomAppBar(
           elevation: 0,
         color: Colors.blue[900],
