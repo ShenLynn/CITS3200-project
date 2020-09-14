34ebe34115d85f9ec516bd5437b4e51a97764351
Shen Sun <shenlynn.sun@gmail.com>
2020-09-14 17:47:56 +0800

    changed main routing for profile

--- asthma_app/lib/main.dart
+++ asthma_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:asthma_app/pages/calendar.dart';
 import 'package:asthma_app/pages/login.dart';
 import 'package:asthma_app/pages/forms_page.dart';
-import 'package:asthma_app/pages/profile.dart';
+import 'package:asthma_app/pages/profilefiles/profile.dart';
 import 'package:asthma_app/pages/help.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:asthma_app/pages/welcome.dart';
